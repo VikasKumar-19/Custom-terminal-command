ed33e60ab5ef53bb9b36e453d3c2a61f85b2306a
Vikas Kumar <vk.pratap98@gmail.com>
2021-05-25 11:50:32 +0530

    Prepared the basic structure of Project

---
+++ Readme.md
@@ -0,0 +1,5 @@
+# Steps for creating tree, organise and help commands
+[] first we have to know that user can only run these commands by entering it.
+[] So we have to take input from user
+[] we use process.argv to take inputs
+[] we can rename the commands later when we will make them global.
---
+++ main.js
@@ -0,0 +1,7 @@
+let inpArr = process.argv.slice(2);
+
+// for tree command we need to enter directory path -> node main.js tree "directory_Path"
+// for organize command we have to do the same -> node main.js organize "directory_Path"
+// for help command -> node.js help
+
+console.log(inpArr);
